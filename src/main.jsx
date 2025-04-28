5956aca9f5ba59b9664bef02eeab7b94706b93a8
yatheeshv <yatheeshvv@gmail.com>
2025-04-28 23:25:33 +0530

    'sdf'

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import InventoryTable from './omponents/InventoryTable.jsx';
+import App from './App'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <InventoryTable />
+    <App /> 
   </React.StrictMode>
 );
